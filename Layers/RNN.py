9ba638f8cf0a17935be1c9bcc888493ac1e14a2c
egeozkoc <egeozkoc@hotmail.com>
2025-02-11 14:02:42 -0500

    Update RNN.py

--- Layers/RNN.py
+++ Layers/RNN.py
@@ -4,8 +4,21 @@ from Layers.TanH import TanH
 from Layers.Sigmoid import Sigmoid
 from Optimization.Optimizers import Sgd
 import numpy as np
+
 class RNN(BaseLayer):
+    """
+    Implements a Recurrent Neural Network (RNN) layer.
+    """
+    
     def __init__(self, input_size, hidden_size, output_size):
+        """
+        Initializes the RNN layer.
+
+        Args:
+            input_size (int): Number of input features.
+            hidden_size (int): Number of hidden units.
+            output_size (int): Number of output features.
+        """
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -21,71 +34,114 @@ class RNN(BaseLayer):
         self.optimizer = None
 
     def forward(self, input_tensor):
+        """
+        Performs the forward pass through the RNN.
+
+        Args:
+            input_tensor (numpy.ndarray): Input tensor of shape (sequence_length, input_size).
+
+        Returns:
+            numpy.ndarray: Output tensor of shape (sequence_length, output_size).
+        """
         self.weights = self.fc1.weights
-        if self.memorize == False:
+        if not self.memorize:
             self.hidden_state = np.zeros(self.hidden_size)
-        self.fc1_input = np.zeros([input_tensor.shape[0], self.input_size + self.hidden_size + 1])
-        self.fc2_input = np.zeros([input_tensor.shape[0], self.hidden_size + 1])
-        self.tanh_output = np.zeros([input_tensor.shape[0], self.hidden_size])
-        self.sigmoid_output = np.zeros([input_tensor.shape[0], self.output_size])
-        output_tensor = np.zeros([input_tensor.shape[0], self.output_size])
-        for i in range(0, input_tensor.shape[0]):
-            input_tensor_tilda = np.concatenate((input_tensor[i], self.hidden_state))
-            input_tensor_tilda = input_tensor_tilda.reshape(1, -1)
+        
+        output_tensor = np.zeros((input_tensor.shape[0], self.output_size))
+        
+        for i in range(input_tensor.shape[0]):
+            input_tensor_tilda = np.concatenate((input_tensor[i], self.hidden_state)).reshape(1, -1)
             self.hidden_state = self.tanh.forward(self.fc1.forward(input_tensor_tilda))
             output_tensor[i] = self.sigmoid.forward(self.fc2.forward(self.hidden_state))
             self.hidden_state = self.hidden_state.ravel()
-            self.fc1_input[i] = self.fc1.get_input()
-            self.fc2_input[i] = self.fc2.get_input()
-            self.tanh_output[i] = self.tanh.get_output()
-            self.sigmoid_output[i] = self.sigmoid.get_output()
+        
         return output_tensor
 
     def backward(self, error_tensor):
+        """
+        Performs the backward pass through the RNN.
+
+        Args:
+            error_tensor (numpy.ndarray): Error tensor from the next layer.
+
+        Returns:
+            numpy.ndarray: Gradient of the input tensor.
+        """
         error_output = np.zeros((error_tensor.shape[0], self.input_size))
         self.gradient_hidden_state = 0
         self.gradient_weights = 0
-        for i in range(error_tensor.shape[0]-1, -1, -1):
-            self.fc1.set_input(self.fc1_input[i].reshape(1, -1))
-            self.fc2.set_input(self.fc2_input[i].reshape(1, -1))
-            self.tanh.set_output(self.tanh_output[i].reshape(1, -1))
-            self.sigmoid.set_output(self.sigmoid_output[i].reshape(1, -1))
+        
+        for i in range(error_tensor.shape[0] - 1, -1, -1):
             backward_step = self.sigmoid.backward(error_tensor[i])
             backward_step = self.fc2.backward(backward_step)
             backward_step += self.gradient_hidden_state
             backward_step = self.tanh.backward(backward_step)
             backward_step = self.fc1.backward(backward_step)
-            error_output[i] = backward_step[:, 0:self.input_size]
-            self.gradient_hidden_state = backward_step[:, self.input_size::]
+            error_output[i] = backward_step[:, :self.input_size]
+            self.gradient_hidden_state = backward_step[:, self.input_size:]
             self.gradient_weights += self.fc1.gradient_weights
+        
         if self.optimizer is not None:
             self.set_weights(self.optimizer.calculate_update(self.get_weights(), self.gradient_weights))
+        
         return error_output
 
     def initialize(self, weights_initializer, bias_initializer):
+        """
+        Initializes weights and biases using the given initializers.
+
+        Args:
+            weights_initializer: Initializer for weights.
+            bias_initializer: Initializer for biases.
+        """
         self.fc1.initialize(weights_initializer, bias_initializer)
         self.fc2.initialize(weights_initializer, bias_initializer)
         self.weights = self.fc1.weights
 
     def get_weights(self):
+        """
+        Returns the current weights of the RNN.
+
+        Returns:
+            numpy.ndarray: Weight matrix of the RNN.
+        """
         return self.fc1.weights
 
     def set_weights(self, weights):
+        """
+        Sets new weights for the RNN.
+
+        Args:
+            weights (numpy.ndarray): New weight matrix.
+        """
         self.fc1.weights = weights
         self.weights = self.fc1.weights
 
     @property
     def weights(self):
+        """
+        Property to get the weights.
+
+        Returns:
+            numpy.ndarray: Weight matrix of the RNN.
+        """
         return self.fc1.weights
 
     @weights.setter
     def weights(self, weights):
+        """
+        Property setter to update the weights.
+
+        Args:
+            weights (numpy.ndarray): New weight matrix.
+        """
         self.fc1.weights = weights
 
     def calculate_regularization_loss(self):
-        regularization_loss = self.optimizer.regularizer.norm(self.weights)
-        return regularization_loss
+        """
+        Computes the regularization loss if a regularizer is used.
 
-
-
-
+        Returns:
+            float: Regularization loss value.
+        """
+        return self.optimizer.regularizer.norm(self.weights)
