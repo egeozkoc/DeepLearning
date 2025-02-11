85f168a9b53a2e525644b45d18f692dfb62d9840
egeozkoc <egeozkoc@hotmail.com>
2025-02-11 13:46:42 -0500

    Update Base.py

--- Layers/Base.py
+++ Layers/Base.py
@@ -1,5 +1,12 @@
 class BaseLayer:
+    """
+    A base class for all layers in the neural network.
+    """
+    
     def __init__(self):
+        """
+        Initializes the BaseLayer with default attributes.
+        """
         self.trainable = False
         self.weights = None
         self.testing_phase = False
